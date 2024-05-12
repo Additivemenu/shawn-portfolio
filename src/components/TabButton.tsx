11bca0394203ae7d30b1ca1dc6f3f1f7935b99c4
Shawn <103415723+Additivemenu@users.noreply.github.com>
2024-05-12 21:18:58 +1000

    feat: added animation for hero, project section (#5)

--- src/components/TabButton.tsx
+++ src/components/TabButton.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 interface TabButtonProps {
   active: boolean;
@@ -6,9 +7,14 @@ interface TabButtonProps {
   children: React.ReactNode;
 }
 
+const variants = {
+  default: { width: 0 },
+  active: { width: "calc(100% - 0.75rem)" },
+};
+
 const TabButton = ({ active, selectTab, children }: TabButtonProps) => {
   const buttonClasses = active
-    ? "text-white border-b border-purple-500"
+    ? "text-white"
     : "text-[#ADB7BE]";
 
   return (
@@ -16,6 +22,11 @@ const TabButton = ({ active, selectTab, children }: TabButtonProps) => {
       <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
         {children}
       </p>
+      <motion.div
+        animate={active ? "active" : "default"}
+        variants={variants}
+        className="h-1 bg-primary-500 mt-2 mr-3"
+      ></motion.div>
     </button>
   );
 };
